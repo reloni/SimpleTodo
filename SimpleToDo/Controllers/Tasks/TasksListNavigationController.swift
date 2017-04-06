15523b60a12e01021e2bbdb61e26cc3105b5270d
Anton Efimenko <reloni@ya.ru>
2017-04-06 17:12:34 +0300

    Fix incorrect blur on screen rotate

--- SimpleToDo/Controllers/Tasks/TasksListNavigationController.swift
+++ SimpleToDo/Controllers/Tasks/TasksListNavigationController.swift
@@ -13,8 +13,6 @@ import RxSwift
 import AMScrollingNavbar
 
 final class TasksListNavigationController : UINavigationController { //: ScrollingNavigationController {
-	let bag = DisposeBag()
-	
 	let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
 	
 	override func viewDidLoad() {
@@ -22,21 +20,11 @@ final class TasksListNavigationController : UINavigationController { //: Scrolli
 		navigationBar.setBackgroundImage(UIImage(), for: .default)
 		navigationBar.isTranslucent = true
 
-		NotificationCenter.default.rx.notification(NSNotification.Name.UIApplicationDidChangeStatusBarFrame)
-			.observeOn(MainScheduler.instance)
-			.subscribe(onNext: { [weak self] notification in
-				guard let object = self else { return }
-				object.blurView.snp.updateConstraints {
-					$0.height.equalTo(object.navigationBar.snp.height).offset(notification.statusBarFrame().height)
-					$0.leading.equalTo(object.view.snp.leading)
-					$0.trailing.equalTo(object.view.snp.trailing)
-				}
-			}).disposed(by: bag)
-
 		view.insertSubview(blurView, belowSubview: navigationBar)
 		
 		blurView.snp.makeConstraints {
-			$0.height.equalTo(navigationBar.snp.height).offset(UIApplication.shared.statusBarFrame.height)
+            $0.top.equalTo(view.snp.topMargin)
+            $0.bottom.equalTo(navigationBar.snp.bottom)
 			$0.leading.equalTo(view.snp.leading)
 			$0.trailing.equalTo(view.snp.trailing)
 		}
