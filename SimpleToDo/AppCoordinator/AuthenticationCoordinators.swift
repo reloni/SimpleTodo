9e85112b5c54ea3cda09c297bf70103f1b84ae0d
Anton Efimenko <reloni@ya.ru>
2017-04-29 22:45:21 +0300

    Place InitialCoordinator into separate file

--- SimpleToDo/AppCoordinator/AuthenticationCoordinators.swift
+++ SimpleToDo/AppCoordinator/AuthenticationCoordinators.swift
@@ -6,78 +6,8 @@
 //  Copyright © 2017 Anton Efimenko. All rights reserved.
 //
 
-import RxDataFlow
 import RxSwift
-
-protocol ApplicationCoordinatorType {
-	var window: UIWindow { get }
-	func handle(_ action: RxActionType, flowController: RxDataFlowController<AppState>) -> Observable<RxStateType>
-}
-
-extension ApplicationCoordinatorType {
-	func handleBase(action: RxActionType, flowController: RxDataFlowController<AppState>, currentViewController controller: UIViewController) -> Observable<RxStateType>? {
-		switch action {
-		case UIAction.showError(let error):
-			showAlert(in: controller, with: error)
-			return .just(flowController.currentState.state)
-		case UIAction.returnToRootController:
-			let coordinator = AuthenticationCoordinator(window: window, controller: AuthenticationController(viewModel: AuthenticationViewModel(flowController: flowController,
-			                                                                                                                                    mode: .logIn)))
-			set(newRootController: coordinator.controller)
-			return .just(flowController.currentState.state.mutation.new(coordinator: coordinator))
-		default: return nil
-		}
-	}
-	
-	func showAlert(in controller: UIViewController, with error: Error) {
-		guard let message = error.uiAlertMessage() else { return }
-		let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-		let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
-		alert.addAction(ok)
-		controller.present(alert, animated: true, completion: nil)
-	}
-	
-	func set(newRootController controller: UIViewController) {
-		transition {
-			self.window.rootViewController = controller
-		}
-	}
-	
-	func set(initialRootController controller: UIViewController) {
-		transition {
-			self.window.rootViewController = controller
-			self.window.makeKeyAndVisible()
-		}
-	}
-	
-	func transition(withDuration duration: TimeInterval = 0.5,
-	                options: UIViewAnimationOptions = [UIViewAnimationOptions.transitionCrossDissolve], animations: @escaping (() -> Void)) {
-		UIView.transition(with: window,
-		                  duration: duration,
-		                  options: options,
-		                  animations: animations,
-		                  completion: nil)
-	}
-}
-
-struct InitialCoordinator : ApplicationCoordinatorType {
-	let window: UIWindow
-	
-	init(window: UIWindow) {
-		self.window = window
-	}
-	
-	func handle(_ action: RxActionType, flowController: RxDataFlowController<AppState>) -> Observable<RxStateType> {
-		switch action {
-		case UIAction.showRootController:
-			let controller = AuthenticationController(viewModel: AuthenticationViewModel(flowController: flowController, mode: .logIn))
-			let coordinator = AuthenticationCoordinator(window: window, controller: controller)
-			set(initialRootController: controller)
-			return .just(flowController.currentState.state.mutation.new(coordinator: coordinator))
-		default: fatalError("Only UIAction.showRootController may be dispatched")
-		}
-	}
-}
+import RxDataFlow
 
 struct AuthenticationCoordinator : ApplicationCoordinatorType {
 	let controller: UIViewController
