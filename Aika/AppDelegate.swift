d4ea95a59bdad37e496b9cfe6bf92f31c0aacae0
Anton Efimenko <reloni@ya.ru>
2017-06-12 19:21:48 +0300

    Add fabric

--- Aika/AppDelegate.swift
+++ Aika/AppDelegate.swift
@@ -11,6 +11,9 @@ import RxHttpClient
 import RxDataFlow
 import RxSwift
 import OneSignal
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -50,6 +53,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}()
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		Fabric.with([Crashlytics.self])
+		
 		window = UIWindow(frame: UIScreen.main.bounds)
 		
 		FileManager.default.createOrUpdateRealmsDirectory()
--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 carthage update RxSwift --cache-builds --platform iOS
 carthage update --cache-builds --platform ios
+
+sh ./Scripts/DownloadFabric.sh
