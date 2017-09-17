a254a51014203f9406071763a6634472385ea647
Anton Efimenko <reloni@ya.ru>
2017-09-17 20:07:13 +0300

    Send device info on login [ch29]

--- Aika/Common/Entities.swift
+++ Aika/Common/Entities.swift
@@ -19,6 +19,24 @@ struct AppConstants {
 //		static let baseUrl = "http://localhost:5000/api/v1"
 	static let host = "aika.cloud"
 	//	static let host = "dev.aika.cloud"
+	
+	static var applicationType: String {
+		switch UI_USER_INTERFACE_IDIOM() {
+		case .pad: return "Aika for iPad"
+		case .phone: return "Aika for iPhone"
+		default: return "Aika for unknown device :)"
+		}
+	}
+	
+	static var applicationVersion: String {
+		let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
+		let buildVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
+		return "\(appVersion) (\(buildVersion))"
+	}
+	
+	static var applicationDeviceInfo: String {
+		return "\(applicationType) \(applicationVersion)"
+	}
 }
 
 enum IconBadgeStyle: String {
