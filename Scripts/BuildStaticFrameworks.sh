4a9e60ffec9408a4fb2c2f942cf06a99c9b5ed72
Anton Efimenko <reloni@ya.ru>
2018-02-03 16:41:50 +0300

    Try to create fat framework

--- Scripts/BuildStaticFrameworks.sh
+++ Scripts/BuildStaticFrameworks.sh
@@ -1,3 +1,4 @@
 eval SYMROOT="$(pwd)/Static"
-#(cd ../Carthage/Checkouts/RxHttpClient && echo $SYMROOT)
-(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphoneos" -configuration Release MACH_O_TYPE=staticlib SYMROOT=$SYMROOT)
+(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphoneos" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=staticlib SYMROOT="$SYMROOT/temp" clean build)
+(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphonesimulator" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=staticlib SYMROOT="$SYMROOT/temp" clean build)
+lipo -create -output "$SYMROOT/RxHttpClient.framework" "$SYMROOT/temp/Release-iphoneos/RxHttpClient.framework/RxHttpClient" "$SYMROOT/temp/Release-iphonesimulator/RxHttpClient.framework/RxHttpClient"
