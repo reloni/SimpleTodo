f879f13b5d27e0afd30b244232c0c3dc3b449d25
Anton Efimenko <reloni@ya.ru>
2018-02-03 20:52:35 +0300

    Working static fat framework

--- Scripts/BuildStaticFrameworks.sh
+++ Scripts/BuildStaticFrameworks.sh
@@ -1,4 +1,17 @@
 eval SYMROOT="$(pwd)/Static"
-(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphoneos" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=staticlib SYMROOT="$SYMROOT/temp" clean build)
-(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphonesimulator" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=staticlib SYMROOT="$SYMROOT/temp" clean build)
-lipo -create -output "$SYMROOT/RxHttpClient.framework" "$SYMROOT/temp/Release-iphoneos/RxHttpClient.framework/RxHttpClient" "$SYMROOT/temp/Release-iphonesimulator/RxHttpClient.framework/RxHttpClient"
+#MACH_O_TYPE=mh_dylib
+MACH_O_TYPE=staticlib
+FRAMEWORK_PATH=RxHttpClient.framework
+FRAMEWORK=RxHttpClient
+(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphoneos" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=$MACH_O_TYPE SYMROOT="$SYMROOT/temp" clean build)
+(cd Carthage/Checkouts/RxHttpClient && xcodebuild -scheme "RxHttpClient" -project "RxHttpClient.xcodeproj" -sdk "iphonesimulator" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=$MACH_O_TYPE SYMROOT="$SYMROOT/temp" clean build)
+#file "$SYMROOT/temp/Release-iphoneos/RxHttpClient.framework/RxHttpClient"
+#file "$SYMROOT/temp/Release-iphonesimulator/RxHttpClient.framework/RxHttpClient"
+#cp -R "$SYMROOT/Release-iphoneos/RxHttpClient.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+
+cp -RL $SYMROOT/temp/Release-iphoneos $SYMROOT/Release-universal
+cp -RL $SYMROOT/temp/Release-iphonesimulator/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/* $SYMROOT/Release-universal/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule
+lipo -create $SYMROOT/temp/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $SYMROOT/temp/Release-iphonesimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $SYMROOT/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
+
+
+#lipo -create -output "$SYMROOT/RxHttpClient.framework/RxHttpClient" "$SYMROOT/temp/Release-iphoneos/RxHttpClient.framework/RxHttpClient" "$SYMROOT/temp/Release-iphonesimulator/RxHttpClient.framework/RxHttpClient"
