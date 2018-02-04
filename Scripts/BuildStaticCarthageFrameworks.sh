0a4495d18e64d9985b878a94669428f2eabccc3d
Anton Efimenko <reloni@ya.ru>
2018-02-04 19:04:53 +0300

    Script for building all frameworks

--- Scripts/BuildStaticCarthageFrameworks.sh
+++ Scripts/BuildStaticCarthageFrameworks.sh
@@ -2,5 +2,39 @@
 
 set -e
 
-sh ./Scripts/BuildStaticFramework.sh RxHttpClient RxHttpClient RxHttpClient
-sh ./Scripts/BuildStaticFramework.sh RxDataFlow RxDataFlow RxDataFlow-iOS
+#carthage update --no-build --no-use-binaries
+mkdir -p Carthage/Build/iOS
+
+#sh ./Scripts/BuildStaticFramework.sh RxSwift RxSwift RxSwift-iOS
+#sh ./Scripts/BuildStaticFramework.sh RxSwift RxCocoa RxCocoa-iOS
+#sh ./Scripts/BuildStaticFramework.sh SnapKit SnapKit SnapKit
+#sh ./Scripts/BuildStaticFramework.sh SimpleKeychain SimpleKeychain SimpleKeychain-iOS
+
+ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/RxGesture/Carthage"
+#sh ./Scripts/BuildStaticFramework.sh RxGesture/RxGesture RxGesture RxGesture-iOS
+
+ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/RxHttpClient/Carthage"
+#sh ./Scripts/BuildStaticFramework.sh RxHttpClient RxHttpClient RxHttpClient
+
+ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/RxDataFlow/Carthage"
+#sh ./Scripts/BuildStaticFramework.sh RxDataFlow RxDataFlow RxDataFlow-iOS
+
+ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/RxDataSources/Carthage"
+#sh ./Scripts/BuildStaticFramework.sh RxDataSources Differentiator Differentiator
+#sh ./Scripts/BuildStaticFramework.sh RxDataSources RxDataSources RxDataSources
+
+#ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/realm-cocoa/Carthage"
+#sh ./Scripts/BuildStaticFramework.sh realm-cocoa Realm "Realm iOS static" -static
+#sh ./Scripts/BuildStaticFramework.sh realm-cocoa RealmSwift RealmSwift
+
+#sh ./Scripts/BuildStaticFramework.sh OneSignal-iOS-SDK/iOS_SDK OneSignal OneSignal-Dynamic
+
+#sh ./Scripts/BuildStaticFramework.sh Motion Motion "Motion iOS"
+
+ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/Material/Carthage"
+#sh ./Scripts/BuildStaticFramework.sh Material Material "Material"
+
+#sh ./Scripts/BuildStaticFramework.sh JWTDecode.swift JWTDecode "JWTDecode-iOS"
+
+ln -sf "$(pwd)/Carthage/Build" "$(pwd)/Carthage/Checkouts/Auth0.swift/Carthage"
+sh ./Scripts/BuildStaticFramework.sh Auth0.swift Auth0 "Auth0.iOS"
--- Scripts/BuildStaticFramework.sh
+++ Scripts/BuildStaticFramework.sh
@@ -6,16 +6,20 @@ CARTHAGE_PATH="$1"
 FRAMEWORK="$2"
 FRAMEWORK_PATH="$2.framework"
 SCHEME="$3"
+EXPORT_PATH_SUFFIX="$4"
 
-eval SYMROOT="$(pwd)/Static"
+eval SYMROOT="$(pwd)/Carthage/Build"
 
 MACH_O_TYPE=staticlib
 (cd Carthage/Checkouts/$CARTHAGE_PATH && xcodebuild -scheme "$SCHEME" -sdk "iphoneos" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=$MACH_O_TYPE SYMROOT="$SYMROOT/temp" build)
 (cd Carthage/Checkouts/$CARTHAGE_PATH && xcodebuild -scheme "$SCHEME" -sdk "iphonesimulator" -configuration Release ONLY_ACTIVE_ARCH=NO MACH_O_TYPE=$MACH_O_TYPE SYMROOT="$SYMROOT/temp" build)
 
-cp -RL $SYMROOT/temp/Release-iphoneos/ $SYMROOT/universal
-cp -RL $SYMROOT/temp/Release-iphonesimulator/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/* $SYMROOT/universal/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule
-lipo -create $SYMROOT/temp/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $SYMROOT/temp/Release-iphonesimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $SYMROOT/universal/$FRAMEWORK_PATH/$FRAMEWORK
+cp -RL $SYMROOT/temp/Release-iphoneos$EXPORT_PATH_SUFFIX/ $SYMROOT/iOS
+if [ -d "$SYMROOT/temp/Release-iphonesimulator$EXPORT_PATH_SUFFIX/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule" ]; then
+cp -RL $SYMROOT/temp/Release-iphonesimulator$EXPORT_PATH_SUFFIX/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/* $SYMROOT/iOS/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule
+fi
+#cp -RL $SYMROOT/temp/Release-iphonesimulator/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/* $SYMROOT/iOS/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule
+lipo -create $SYMROOT/temp/Release-iphoneos$EXPORT_PATH_SUFFIX/$FRAMEWORK_PATH/$FRAMEWORK $SYMROOT/temp/Release-iphonesimulator$EXPORT_PATH_SUFFIX/$FRAMEWORK_PATH/$FRAMEWORK -output $SYMROOT/iOS/$FRAMEWORK_PATH/$FRAMEWORK
 #cp -rf $SYMROOT/universal/$FRAMEWORK_PATH $SYMROOT
 rm -rf $SYMROOT/temp
 #rm -rf $SYMROOT/universal
