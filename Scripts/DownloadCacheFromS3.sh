ffd7afd9e9bbb8b60196c1448d152929b75ed37e
Anton Efimenko <aefimenko@moysklad.ru>
2018-02-27 10:05:38 +0300

    Modify download script

--- Scripts/DownloadCacheFromS3.sh
+++ Scripts/DownloadCacheFromS3.sh
@@ -3,4 +3,4 @@ set -e
 
 aws s3 sync s3://app-build-caches/Aika/Carthage/Build/iOS ./Carthage/Build/iOS/
 
-#(cd Carthage/Build/iOS/ && for i in `find . | grep -E ".framework.tar.gz$"`; do tar -xzf "$i" ; done)
+(cd Carthage/Build/iOS/ && for i in `find . | grep -E ".framework.tar.gz$"`; do tar -xzf "$i" ; done)
